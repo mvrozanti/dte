453dd1fdc5ca38bfa5594362135eb08c61fc6bf3
Marcelo <mvrozanti@hotmail.com>
2021-01-24 19:44:15 -0300

    add setup_requires

--- setup.py
+++ setup.py
@@ -5,24 +5,19 @@ import os
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-libdir = os.path.dirname(os.path.realpath(__file__))
-requirements_path = libdir + '/requirements.txt'
-install_requires = []
-if os.path.isfile(requirements_path):
-    with open(requirements_path) as f:
-        install_requires = f.read().splitlines()
-
 setuptools.setup(
      name='dte',  
-     version='0.0.4',
+     version='0.0.56',
      author="Marcelo V. Rozanti",
      author_email="mvrozanti@hotmail.com",
      description="Date Time Expressions",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/mvrozanti/dte",
-     packages=setuptools.find_packages(),
-     install_requires=install_requires,
+     packages=setuptools.find_packages('dte'),
+     install_requires=[
+         'python-dateutil', 'ply'
+         ],
      scripts=[
          'dte/dte',
          ],
