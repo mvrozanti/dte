0ccd63066d803de55f35e43bdff9a2e38512d0fe
Marcelo <mvrozanti@hotmail.com>
2021-01-24 23:55:11 -0300

    fixed time declaration and added tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
      name='dte',  
-     version='0.0.56',
+     version='0.0.57',
      author="Marcelo V. Rozanti",
      author_email="mvrozanti@hotmail.com",
      description="Date Time Expressions",
