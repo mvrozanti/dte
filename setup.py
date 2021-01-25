1e07eba61f105345f508f1cf04cfea061a2d3026
Marcelo <mvrozanti@hotmail.com>
2021-01-25 13:32:10 -0300

    add automatic version bump on pypi distribution

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
      name='dte',  
-     version='0.0.57',
+     version='0.0.58',
      author="Marcelo V. Rozanti",
      author_email="mvrozanti@hotmail.com",
      description="Date Time Expressions",
