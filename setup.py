f63367b4ce4d2656e4d91f2089c2543a339ada87
Marcelo <mvrozanti@hotmail.com>
2021-01-24 22:04:22 -0300

    add python versions

--- setup.py
+++ setup.py
@@ -28,6 +28,9 @@ setuptools.setup(
          "Programming Language :: Python :: 3.4",
          "Programming Language :: Python :: 3.5",
          "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "Programming Language :: Python :: 3.9",
          "Programming Language :: Python :: 3 :: Only",
          ],
  )
