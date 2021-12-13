e0f9461553846a2911606d07ad096826fcfab98e
Marcelo <mvrozanti@hotmail.com>
2021-12-12 23:50:30 -0300

    Fix wait expressions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
      name='dte',  
-     version='0.3.0',
+     version='0.3.1',
      author="Marcelo V. Rozanti",
      author_email="mvrozanti@hotmail.com",
      description="Date Time Expressions",
